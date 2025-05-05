6f7da5f20431f0582e6523c4802d243a4cf94cc7
ijsbol <scrumpy@weeb.email>
2025-05-06 00:28:52 +0100

    feat: bump version 0.2.0 -> 0.2.1
    Co-Authored-By: kit <188646017+ki-ty@users.noreply.github.com>

--- httpcord/__init__.py
+++ httpcord/__init__.py
@@ -34,5 +34,5 @@ __all__: Final[tuple[str, ...]] = (
     "Interaction",
 )
 
-__version__: Final[str] = "0.2.0"
+__version__: Final[str] = "0.2.1"
 __author__: Final[str] = "Isabelle Phoebe <izzy@uwu.gal>"
