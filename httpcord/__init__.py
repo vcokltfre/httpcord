5b073edf8a9a346fe5c5b8107fc505180969de0b
ijsbol <scrumpy@weeb.email>
2025-05-31 18:04:54 +0100

    feat: bump version

--- httpcord/__init__.py
+++ httpcord/__init__.py
@@ -34,5 +34,5 @@ __all__: Final[tuple[str, ...]] = (
     "Interaction",
 )
 
-__version__: Final[str] = "0.2.2"
+__version__: Final[str] = "0.3.0"
 __author__: Final[str] = "Isabelle Phoebe <izzy@uwu.gal>"
